ecb6db34e8380e00e507b76ba245a2f99535f6ef
Kristina <krismetodieva123@gmail.com>
2023-05-26 10:02:02 +0300

    add cube page is displayed
    cube controller

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express'); //get express by express library
 const expressConfig = require('./config/expressConfig');
 const hbsConfig = require('./config/hbsConfig');
 const homeController = require('./controllers/homeControllers');
+const cubeController = require('./controllers/cubeController');
 
 const app = express(); //create instance
 const PORT = 5000; //port value
@@ -11,5 +12,6 @@ expressConfig(app);
 hbsConfig(app);
 
 app.use(homeController);
+app.use('/cubes', cubeController);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
