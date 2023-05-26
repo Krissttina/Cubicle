71134b7fb3697cdb5a4266b9ce61779fd8d35e5e
Kristina <krismetodieva123@gmail.com>
2023-05-26 10:07:39 +0300

    get data from 'add'

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,8 +1,13 @@
 const router = require('express').Router(); 
 
 //Path /cubes/create
-router.get('/create', (req, res) => {
+router.get('/create', (req, res) => { //action#1 serve form
     res.render('create');
+});
+
+router.post('/create', (req, res) => { //action#2 accept data after form is submited
+    console.log(req.body); // get data
+    res.redirect('/');
 });
 
 module.exports = router;
