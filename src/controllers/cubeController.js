415b5152c6a8620909af3d70cc75d5e015163155
Kristina <krismetodieva123@gmail.com>
2023-05-30 19:09:15 +0300

    attach accessory

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router(); 
 
 const cubeManager = require('../managers/cubeManager');
+const accessoryManager = require('../managers/accessoryManager');
 
 //Path /cubes/create
 router.get('/create', (req, res) => { //action#1 serve form
@@ -40,8 +41,12 @@ router.get('/:cubeId/details', async (req, res) => {
 
 //accessory attach
 
-router.get('/:cubId/attach-accessory', (req, res) => {
-    res.render('accessory/attach');
+router.post('/:cubId/attach-accessory', async (req, res) => {
+    const cube = await cubeManager.getOne(req.params.cubeId).lean();
+    const accessories = await accessoryManager.getAll().lean();
+    const hasAccessories = accessories.length > 0;
+
+    res.render('accessory/attach', { cube, accessories, hasAccessories });
 });
 
 module.exports = router;
