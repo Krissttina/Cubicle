e77674dd5fcc66de1e108432778ed5c31d45f042
KristinaMetodieva <kristinametodieva621@gmail.com>
2023-05-30 13:23:46 +0300

    add post action for create accessory

--- src/controllers/accessoryContr.js
+++ src/controllers/accessoryContr.js
@@ -2,6 +2,14 @@ const router = require('express').Router();
 
 router.get('/create', (req, res) => {
     res.render('accessory/create');
+});
+
+router.post('/create', (req, res) => {
+    const body = req.body;
+    console.log(body);
+    //TODO: add accessor
+
+    res.redirect('/');
 })
 
 module.exports = router;
