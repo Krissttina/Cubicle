12c1c77644506a195e786807c02cbdc6865fbd3c
Kristina <krismetodieva123@gmail.com>
2023-06-09 09:22:39 +0300

    edit view and functionality

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -68,10 +68,19 @@ router.post('/:cubeId/delete', async (req, res) => {
     await cubeManager.delete(req.params.cubeId);
 
     res.redirect('/');
-})
+});
 
-router.get('/:cubeId/edit', (req, res) => {
-    res.render('cube/edit');//where cube is in the folder src/views and edit is the file to load
+router.get('/:cubeId/edit', async(req, res) => {
+    const cube = await cubeManager.getOne(req.params.cubeId).lean();
+    res.render('cube/edit', { cube });//where cube is in the folder src/views and edit is the file to load
+});
+
+router.post('/:cubeId/edit', async (req, res) => {
+    const cubeData = req.body;
+
+    await cubeManager.update(req.params.cubeId, cubeData);
+
+    res.redirect(`/cubes/${req.params.cubeId}/details`);
 });
 
 module.exports = router;
